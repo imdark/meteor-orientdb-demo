3bbbc4964d2a485a9372152351f33b7378038982
unknown <mkris@zahav.net.il>
2015-07-04 01:30:40 +0300

    forgot to add fixed markupp file and server

--- readme.md
+++ readme.md
@@ -1,1 +1,31 @@
-# OrientDB  Leaderboard Example
+OrientDB  Leaderboard Example
+=============================
+
+## install
+
+###Prerequisites:
+* node.js [https://nodejs.org/](https://nodejs.org/)
+* Meteor.js [https://www.meteor.com/install](https://www.meteor.com/install)
+* orientdb [http://orientdb.com/download/](http://orientdb.com/download/)
+
+Start the orientDB server by running c:\orientInstall\bin\server.bat on windows or ~/orientInstall/bin/server.sh on linux and browse to localhost:2480
+you will also need the provide an orientDb user and password for this demo to work, you choose them when first running the server.bat file, you need to add them to the top of server/server.js
+like so:
+
+```js
+
+var orientdb = new Meteor.LiveOrientDB({
+  host: 'localhost',
+  port: '2424',
+  username: 'root',
+  password: '12123333',
+  database: 'players'
+});
+
+
+```
+
+
+## References
+* [https://github.com/orientechnologies/orientdb/issues/3602](The original github issue)
+* [https://github.com/orientechnologies/orientdb-docs/blob/master/Live-Query.md#livequery-in-nodejs](documentation explaining the use of live query in orientdev docs)
--- server/server.js
+++ server/server.js
@@ -7,61 +7,42 @@ var orientdb = new Meteor.LiveOrientDB({
   database: 'players'
 });
 
-
-
-Meteor.publish('test', function(){
+Meteor.publish('test', function() {
   return orientdb.select(
-    'select * from player', {params:{table:"player"}}
+    'select * from players', {params:{table:"player"}}
   );
 });
 
-///////////////////////////////////////////////////////////////////
-var liveDb = new LiveMysql({
-  host: 'localhost',
-  user: 'root',
-  password: '235711',
-  database: 'leaderboard'
-});
+// Meteor.publish('allPlayers', function(){
+//   return orientdb.select(
+//     'SELECT * FROM players ORDER BY score DESC',
+//     {params:{ table: 'player' } }
+//   );
+// });
 
-var mysqlresult = liveDb.select(
-  'SELECT * FROM players ORDER BY score DESC',
-  [ { table: 'players' } ]
-);
-
-
-Meteor.publish('allPlayers', function(){
-  return liveDb.select(
-    'SELECT * FROM players ORDER BY score DESC',
-    [ { table: 'players' } ]
-  );
-});
-
-Meteor.publish('playerScore', function(name){
-  return liveDb.select(
-    'SELECT id, score FROM players WHERE name = ' + liveDb.db.escape(name),
-    [
-      {
-        table: 'players',
-        condition: function(row, newRow){
-          return row.name === name;
-        }
-      }
-    ]
-  );
-});
+// Meteor.publish('playerScore', function(name){
+//   return orientdb.select(
+//     'SELECT id, score FROM players WHERE name = ' + liveDb.db.escape(name),
+//     [
+//       {
+//         table: 'players',
+//         condition: function(row, newRow){
+//           return row.name === name;
+//         }
+//       }
+//     ]
+//   );
+// });
 
 Meteor.methods({
   'incScore': function(){
-      orientdb.execute('INSERT into player set name=:name, score=:score', {params:{name:'abc', score:100}});
+      orientdb.execute('INSERT into players set name=:name, score=:score', {params:{name:'abc', score:100}});
     },
   'delScore': function(){
-      orientdb.execute('DELETE vertex from player where name=:name', {params:{name:'abc'}});
+      orientdb.execute('DELETE vertex from players where name=:name', {params:{name:'abc'}});
     },
 
   'updScore': function(options){
       orientdb.execute('UPDATE #12:1 SET score=:score', options);
     },
-
-
-
 });
